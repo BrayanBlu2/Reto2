20642d2d1e2cd2c257feed6fe511e6907b553fcf
BrayanBlu2 <br.arana@aulanz.net>
2024-01-29 09:30:03 +0100

    cambios de las paginas

---
+++ pasarImagenes.js
@@ -0,0 +1,13 @@
+const imagenes = ["/fotoshotel/hotel.jpg", "/fotoshotel/piscina.jpg", "/fotoshotel/habitación2.jpg"];
+let indiceActual = 0;
+
+function cambiarImagen(direccion) {
+  if (direccion === "anterior") {
+    indiceActual = (indiceActual - 1 + imagenes.length) % imagenes.length;
+  } else if (direccion === "siguiente") {
+    indiceActual = (indiceActual + 1) % imagenes.length;
+  }
+
+  const imagen = document.getElementById("imagenActual");
+  imagen.src = imagenes[indiceActual];
+}
