5b8797fa8639c748825f0ef8132098115473a3e4
BrayanBlu2 <br.arana@aulanz.net>
2024-01-30 08:34:33 +0100

    quitamos chunches de js

--- pasarImagenes.js
+++ pasarImagenes.js
@@ -12,19 +12,5 @@ function cambiarImagen(direccion) {
   imagen.src = imagenes[indiceActual];
 }
 
-function verificarFormularioReserva() {
-  // Obtener los valores de los campos del formulario 1
-  var Fecha = document.getElementById("Fecha").value;
 
-  // Verificar si todos los campos del formulario 1 están llenos
-  if (Fecha.trim() !== "") {
-    // Habilitar el formulario 2 para su envío
-    document.getElementById("formulariocliente").addEventListener("submit", function () {
-      alert("Formulario 1 lleno. Enviando Formulario 2.");
-    });
-  } else {
-    // Mostrar un mensaje de error si el formulario 1 no está completamente lleno
-    alert("Por favor, complete todos los campos del Formulario 1 antes de enviar el Formulario 2.");
-  }
-}
 
