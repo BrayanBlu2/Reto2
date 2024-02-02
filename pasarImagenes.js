f879ed12ad5fd4dc54ed1606dc24dd6b87076ded
BrayanBlu2 <br.arana@aulanz.net>
2024-02-02 08:48:53 +0100

    cambios de java

--- pasarImagenes.js
+++ pasarImagenes.js
@@ -1,16 +1,43 @@
-const imagenes = ["/fotoshotel/hotel.jpg", "/fotoshotel/piscina.jpg", "/fotoshotel/habitación2.jpg"];
-let indiceActual = 0;
+const imagenes1 = ["/fotoshotel/hotel.jpg", "/fotoshotel/piscina.jpg", "/fotoshotel/habitación2.jpg"];
+let indiceActual1 = 0;
 
-function cambiarImagen(direccion) {
+function cambiarImagenPiscina(direccion) {
   if (direccion === "anterior") {
-    indiceActual = (indiceActual - 1 + imagenes.length) % imagenes.length;
+    indiceActual1 = (indiceActual1 - 1 + imagenes1.length) % imagenes1.length;
   } else if (direccion === "siguiente") {
-    indiceActual = (indiceActual + 1) % imagenes.length;
+    indiceActual1 = (indiceActual1 + 1) % imagenes1.length;
   }
 
   const imagen = document.getElementById("imagenActual");
-  imagen.src = imagenes[indiceActual];
+  imagen.src = imagenes1[indiceActual1];
 }
 
 
+const imagenes2 = ["/fotoshotel/hotel.jpg", "/fotoshotel/piscina.jpg", "/fotoshotel/habitación2.jpg"];
+let indiceActual2 = 0;
+
+function cambiarImagenGimnasio(direccion) {
+  if (direccion === "anterior") {
+    indiceActual2 = (indiceActual2 - 1 + imagenes2.length) % imagenes2.length;
+  } else if (direccion === "siguiente") {
+    indiceActual2 = (indiceActual2 + 1) % imagenes2.length;
+  }
+
+  const imagen = document.getElementById("imagenActual");
+  imagen.src = imagenes2[indiceActual2];
+}
+
+const imagenes3 = ["/fotoshotel/hotel.jpg", "/fotoshotel/piscina.jpg", "/fotoshotel/habitación2.jpg"];
+let indiceActual3 = 0;
+
+function cambiarImagenRestaurante(direccion) {
+  if (direccion === "anterior") {
+    indiceActual3 = (indiceActual3 - 1 + imagenes3.length) % imagenes3.length;
+  } else if (direccion === "siguiente") {
+    indiceActual3 = (indiceActual3 + 1) % imagenes3.length;
+  }
+
+  const imagen = document.getElementById("imagenActual");
+  imagen.src = imagenes3[indiceActual3];
+}
 
